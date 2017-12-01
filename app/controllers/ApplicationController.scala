f83a776fe51511ae910d346d05038b9757ccdacc
Kirill Meng <kirill.meng@hotmail.de>
2017-12-01 14:14:52 +0100

    add application controller
    	- add route for savestock
    	- remove template stoff from test script

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,5 +1,31 @@
 package controllers
+import play.api.mvc._
+import model.{Teilnehmer, TeilnehmerTable, Firma, FirmaTable}
+import slick.jdbc.MySQLProfile.api._
+import slick.lifted.TableQuery
 
-class ApplicationController {
+class ApplicationController(cc: ControllerComponents) extends AbstractController(cc){
+  lazy val teilnehmer = TableQuery[TeilnehmerTable]
+  lazy val firma = TableQuery[FirmaTable]
+  val db = Database.forConfig("onedayvr")
 
+  def index():Result={
+    db.createSession()
+    Results.ok(views.html.index("Hallo"))
+  }
+  def saveStock(): Request ={
+    request =>
+      val json = request.body.asJson.get
+      val stock = json.as[Stock]
+      println(stock)
+      Results.Ok("")
+  }
+
+  def listAll():Unit={
+    //db.run(table.result)
+  }
+
+  def insertTeilnehmer(t: Teilnehmer): Unit ={
+    db.run(teilnehmer += t)
+  }
 }
