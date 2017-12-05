07c1b8c2bc845f62c49f8443524ff8a5b4d3b98d
Andreas M. Reumschuessel <andreas@reumschuessel.de>
2017-12-05 15:57:17 +0100

    changed firmenname from null to privat

--- sql_data/dummy_data.sql
+++ sql_data/dummy_data.sql
@@ -1,7 +1,7 @@
 INSERT INTO firma (firmenname, strasse, hausnummer, plz, ort, land) VALUES ("Klaus Bärbel AG", "Güllestraße", "3b", "6430 ", "Schwyz", "Schweiz");
 INSERT INTO firma (firmenname, strasse, hausnummer, plz, ort, land) VALUES ("Holzmann und Klaus KG", "Zur alten Eisenware", "17", "10969", "Berlin", "Deutschland");
 INSERT INTO firma (firmenname, strasse, hausnummer, plz, ort, land) VALUES ("Silberfluss GmbH", "Jägerweg", "4", "40213 ", "Düsseldorf", "Deutschland");
-INSERT INTO firma (strasse, hausnummer, plz, ort, land) VALUES ("Kanisterkopf", "48", "1337 ", "Leetdorf", "Singapur");
+INSERT INTO firma (firmenname, strasse, hausnummer, plz, ort, land) VALUES ("privat", "Kanisterkopf", "48", "1337 ", "Leetdorf", "Singapur");
 
 INSERT INTO teilnehmer (nachname, vorname, email, fnummer) VALUES ("Schachtschneider", "Peter", "xxK1ll4_g36@yahoo.com", 1);
 INSERT INTO teilnehmer (nachname, vorname, email, fnummer) VALUES ("Hefeteig", "Korinna", "luftkeks6@gmail.com", 1);
