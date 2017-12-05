9b715e185ad97ccf55fb976e3cd55b7b5f1eaa4d
Andreas M. Reumschuessel <andreas@reumschuessel.de>
2017-12-05 15:57:59 +0100

    adjusted selects to fit for the new firmenname for private participants

--- sql_data/sample_selects.sql
+++ sql_data/sample_selects.sql
@@ -2,7 +2,7 @@
 SELECT firmenname, nachname, vorname, email, strasse, hausnummer, plz, ort, land FROM teilnehmer JOIN firma ON teilnehmer.fnummer = firma.fnummer;
 
 /* Alle Privatpersonen */
-SELECT nachname, vorname, email, strasse, hausnummer, plz, ort, land FROM teilnehmer JOIN firma ON teilnehmer.fnummer = firma.fnummer AND firma.firmenname IS NULL;
+SELECT nachname, vorname, email, strasse, hausnummer, plz, ort, land FROM teilnehmer JOIN firma ON teilnehmer.fnummer = firma.fnummer AND firma.firmenname = "privat";
 
 /* Teilnehmer pro Firma, ohne Privatpersonen */
-SELECT firmenname, COUNT(*) AS anz_teilnehmer FROM teilnehmer JOIN firma ON teilnehmer.fnummer = firma.fnummer AND firmenname IS NOT NULL GROUP BY firmenname;
+SELECT firmenname, COUNT(*) AS anz_teilnehmer FROM teilnehmer JOIN firma ON teilnehmer.fnummer = firma.fnummer AND firmenname != "privat" GROUP BY firmenname;
