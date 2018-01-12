d06c53c4e115a1630c4580d9443dcf0efc713ff9
Kirill Meng <kimeng@htwg-konstanz.de>
2018-01-12 14:54:29 +0100

    surround insert statements with if else to avoid inserting despite max number guests reached

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -16,7 +16,7 @@ class ApplicationController @Inject()(cc: ControllerComponents) extends Abstract
   lazy val firmen = TableQuery[FirmaTable]
   val db = Database.forConfig("onedayvr")
   val DEFAULT_FIRMENID = 0
-  val MAXGUESTS = 250
+  val MAXGUESTS = 21
 
   def index=Action{
     db.createSession()
@@ -33,57 +33,59 @@ class ApplicationController @Inject()(cc: ControllerComponents) extends Abstract
     if(json == null){
       state.setNull()
     }else{
-      if(listAllTeilNehmer() == MAXGUESTS)
+      if(listAllTeilNehmer() == MAXGUESTS){
         state.statuscode = state.FAIL
-      val anrede = getJsonString((json \ "anrede").get.toString())
-      state.addDataEntry(anrede, "Frau|Herr")
+      }else{
+        val anrede = getJsonString((json \ "anrede").get.toString())
+        state.addDataEntry(anrede, "Frau|Herr")
 
-      val email = getJsonString((json \ "email").get.toString())
-      state.addDataEntry(email, "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+        val email = getJsonString((json \ "email").get.toString())
+        state.addDataEntry(email, "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
 
-      var firmenname:String = "privat"
+        var firmenname:String = "privat"
 
-      try{
-        firmenname = getJsonString((json \ "firmaOptions").get.toString())
-        state.addDataEntry(firmenname, "^[A-z|üÜ|öÖ|äÄ|\\s|\\-\\.]{2,}")
-      }catch{
-        case ex: NoSuchElementException => println("[info] use privat teilnehmer")
-      }
-
-      val nachname = getJsonString((json \ "nachname").get.toString())
-      state.addDataEntry(nachname, "^[A-z|üÜ|öÖ|äÄ|\\s|\\-\\.]{2,}")
-
-      val ort = getJsonString((json \ "ort").get.toString())
-      state.addDataEntry(ort, "^[A-z|üÜ|öÖ|äÄ|\\s|\\-\\.]{2,}")
-
-      val plz = getJsonString((json \ "postleitzahl").get.toString())
-      state.addDataEntry(plz, "^[0-9]+")
-
-      val strasse = getJsonString((json \ "strasse").get.toString())
-      state.addDataEntry(strasse, "^[A-z|üÜ|öÖ|äÄ|ß|\\s|\\-\\.|0-9]{2,}")
-
-      val telefon = getJsonString((json \ "telefon").get.toString())
-      state.addDataEntry(telefon, "^\\+{0,1}[0-9]*")
-
-      val titel = getJsonString((json \ "titel").get.toString())
-      state.addDataEntry(titel, "(Professor|Dr.|Professor Dr.){0,1}")
-
-      val vorname = getJsonString((json \ "vorname").get.toString())
-      state.addDataEntry(vorname, "^[A-z|üÜ|öÖ|äÄ|\\s|\\-\\.]{2,}")
-
-      //validate data
-      while(state.hasNext()){
-        state.validate()
-      }
-
-      //if data is invalid then reject it
-      if(state.statuscode == state.OK)
-      {
-        val fnummer:Int = insertFirma(Firma(firmenname, DEFAULT_FIRMENID, strasse, plz, ort))
         try{
-          insertTeilnehmer(Teilnehmer(vorname, nachname, email, fnummer, titel, anrede, telefon))
-        } catch{
-          case ex: Exception => FAILED_DEPENDENCY
+          firmenname = getJsonString((json \ "firmaOptions").get.toString())
+          state.addDataEntry(firmenname, "^[A-z|üÜ|öÖ|äÄ|\\s|\\-\\.]{2,}")
+        }catch{
+          case ex: NoSuchElementException => println("[info] use privat teilnehmer")
+        }
+
+        val nachname = getJsonString((json \ "nachname").get.toString())
+        state.addDataEntry(nachname, "^[A-z|üÜ|öÖ|äÄ|\\s|\\-\\.]{2,}")
+
+        val ort = getJsonString((json \ "ort").get.toString())
+        state.addDataEntry(ort, "^[A-z|üÜ|öÖ|äÄ|\\s|\\-\\.]{2,}")
+
+        val plz = getJsonString((json \ "postleitzahl").get.toString())
+        state.addDataEntry(plz, "^[0-9]+")
+
+        val strasse = getJsonString((json \ "strasse").get.toString())
+        state.addDataEntry(strasse, "^[A-z|üÜ|öÖ|äÄ|ß|\\s|\\-\\.|0-9]{2,}")
+
+        val telefon = getJsonString((json \ "telefon").get.toString())
+        state.addDataEntry(telefon, "^\\+{0,1}[0-9]*")
+
+        val titel = getJsonString((json \ "titel").get.toString())
+        state.addDataEntry(titel, "(Professor|Dr.|Professor Dr.){0,1}")
+
+        val vorname = getJsonString((json \ "vorname").get.toString())
+        state.addDataEntry(vorname, "^[A-z|üÜ|öÖ|äÄ|\\s|\\-\\.]{2,}")
+
+        //validate data
+        while(state.hasNext()){
+          state.validate()
+        }
+
+        //if data is invalid then reject it
+        if(state.statuscode == state.OK)
+        {
+          val fnummer:Int = insertFirma(Firma(firmenname, DEFAULT_FIRMENID, strasse, plz, ort))
+          try{
+            insertTeilnehmer(Teilnehmer(vorname, nachname, email, fnummer, titel, anrede, telefon))
+          } catch{
+            case ex: Exception => FAILED_DEPENDENCY
+          }
         }
       }
     }
