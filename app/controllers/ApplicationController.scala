e090432570a6677ec9e9de07872f0d6f18cb775a
Andreas M. Reumschuessel <andreas@reumschuessel.de>
2018-01-15 15:34:27 +0100

    Fixed email and phone validation

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -40,7 +40,7 @@ class ApplicationController @Inject()(cc: ControllerComponents) extends Abstract
         state.addDataEntry(anrede, "Frau|Herr")
 
         val email = getJsonString((json \ "email").get.toString())
-        state.addDataEntry(email, "(?:[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+        state.addDataEntry(email, "(?:[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[A-Za-z0-9-]*[A-Za-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
 
         var firmenname:String = "privat"
 
@@ -64,7 +64,7 @@ class ApplicationController @Inject()(cc: ControllerComponents) extends Abstract
         state.addDataEntry(strasse, "^[A-z|üÜ|öÖ|äÄ|ß|\\s|\\-\\.|0-9]{2,}")
 
         val telefon = getJsonString((json \ "telefon").get.toString())
-        state.addDataEntry(telefon, "^\\+{0,1}[0-9]*")
+        state.addDataEntry(telefon, "^\\+{0,1}[0-9\\s]*")
 
         val titel = getJsonString((json \ "titel").get.toString())
         state.addDataEntry(titel, "(Professor|Dr.|Professor Dr.){0,1}")
