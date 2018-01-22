938a408003162f1ea1b3301881c91d88aca4817b
Stefan <stefanleminh@gmail.com>
2018-01-22 12:18:20 +0100

    Fix ticket jquery, fix database inserts

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -46,7 +46,7 @@ class ApplicationController @Inject()(cc: ControllerComponents) extends Abstract
         val email = getJsonString((json \ "email").get.toString())
         state.addDataEntry(email, "(?:[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[A-Za-z0-9-]*[A-Za-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
 
-        val firmaOptions = getJsonString((json \ "email").get.toString())
+        val firmaOptions = getJsonString((json \ "firmaOptions").get.toString())
         state.addDataEntry(firmaOptions, "student|firma|privat")
 
         var firmenname:String = "privat"
@@ -90,7 +90,7 @@ class ApplicationController @Inject()(cc: ControllerComponents) extends Abstract
         {
           val fnummer:Int = insertFirma(Firma(firmenname, DEFAULT_FIRMENID, strasse, plz, ort))
           try{
-            insertTeilnehmer(Teilnehmer(vorname, nachname, email, fnummer, titel, anrede, telefon))
+            insertTeilnehmer(Teilnehmer(vorname, nachname, email, fnummer, titel, anrede, ticket))
             //send email to host
             val sm: HtmlEmail = new HtmlEmail()
             sm.setSmtpPort(ConfigFactory.load().getInt("smtp.port"))
