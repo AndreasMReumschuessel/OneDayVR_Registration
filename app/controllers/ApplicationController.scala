4570ece03da9069af8b229890cfba3da1fd5d6c3
Kirill Meng <kimeng@htwg-konstanz.de>
2018-01-24 12:37:26 +0100

    add config entry to set maximal registrated users

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -16,7 +16,7 @@ class ApplicationController @Inject()(cc: ControllerComponents) extends Abstract
   lazy val firmen = TableQuery[FirmaTable]
   val db = Database.forConfig("onedayvr")
   val DEFAULT_FIRMENID = 0
-  val MAXGUESTS = 250
+  val MAXGUESTS = Integer.parseInt(ConfigFactory.load().getString("latches.MAXGUESTS"))
 
   def index=Action{
     Ok(views.html.index("Hallo"))
